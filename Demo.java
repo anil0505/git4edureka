3f74391c05b5ee2e499b8da304fcfa0833cf10b4
anil0505 <41119802+anil0505@users.noreply.github.com>
2020-10-11 16:12:08 +0530

    Update Demo.java
    Second commit from remote repo

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ public class Demo
 {
   public static void main (String aregs[])
   {
-    System.out.println("Commit in remote branch"); 
+    System.out.println("Commit from remote branch"); 
+    System.out.println("Second commit from remote branch");
   }
 }
